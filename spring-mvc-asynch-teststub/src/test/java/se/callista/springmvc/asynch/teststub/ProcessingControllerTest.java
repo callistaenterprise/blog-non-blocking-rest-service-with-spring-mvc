7029c3826f1b9b003b661eefeb5ffa9692888f7a
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2014-06-07 12:11:10 +0200

    Code for how to test non-blocking services under spring-boot

---
+++ spring-mvc-asynch-teststub/src/test/java/se/callista/springmvc/asynch/teststub/ProcessingControllerTest.java
@@ -0,0 +1,77 @@
+package se.callista.springmvc.asynch.teststub;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by magnus on 07/06/14.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class ProcessingControllerTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProcessingControllerTest.class);
+
+    @Autowired
+    ProcessingController customerController;
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    WebApplicationContext wac;
+
+    private final String expectedResult = "{\"status\":\"Ok\",\"processingTimeMs\":2000}";
+
+    @Before
+    public void setup(){
+
+        // Process mock annotations
+        MockitoAnnotations.initMocks(this);
+
+        // Setup Spring test in webapp-mode (same config as spring-boot)
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void testProcessBlocking() throws Exception{
+        this.mockMvc.perform(get("/process-blocking?minMs=2000&maxMs=2000"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().string(expectedResult));
+    }
+
+    @Test
+    public void testProcessNonBlocking() throws Exception {
+
+        MvcResult mvcResult = this.mockMvc.perform(get("/process-non-blocking?minMs=2000&maxMs=2000"))
+                .andExpect(request().asyncStarted())
+                .andReturn();
+
+        mvcResult.getAsyncResult();
+
+        this.mockMvc.perform(asyncDispatch(mvcResult))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().string(expectedResult));
+    }
+}
